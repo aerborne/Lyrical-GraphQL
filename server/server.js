986581b39093ad81d9517fda4f5f397fa3a985b1
Karu Atienza <karlthegreat18@gmail.com>
2018-04-27 19:13:42 +0800

    apollo

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://karu:bobobo@ds261479.mlab.com:61479/samplemongo';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
