1107b6a9256799b7617f633290d500870d72e463
Karu Atienza <karlthegreat18@gmail.com>
2018-04-28 17:25:56 +0800

    client mutations

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,14 +3,21 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+    renderSongs(){
+        return this.props.data.songs.map(song=>{
+            return (
+                <li key={song.id} className="collection-item">
+                    {song.title}
+                </li>
+            )
+        })
+    }
     render(){
-        if(this.props.data.songs){
-            console.log(this.props.data.songs)
-        }
+        if(this.props.data.loading){ return (<div></div>); }
         return (
-            <div>
-                SongList
-            </div>
+            <ul className="collection">
+                {this.renderSongs()}
+            </ul>
         );
     }
 }
@@ -18,6 +25,7 @@ class SongList extends Component {
 const query = gql`
     {
         songs{
+            id
             title
         }
     }
